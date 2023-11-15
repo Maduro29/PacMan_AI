c05f55297fb8e2e3a5cf080b7bcc65afee529c6c
hoanganhnth <hoanganhnth2k3@gmail.com>
2023-11-15 23:19:33 +0700

    fix pro2-Q3

--- logic/logic/logicPlan.py
+++ logic/logic/logicPlan.py
@@ -354,7 +354,7 @@ def checkLocationSatisfiability(x1_y1: Tuple[int, int], x0_y0: Tuple[int, int], 
     KB.append(conjoin(map_sent))
 
     "*** BEGIN YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
     KB.append(pacphysicsAxioms(0, all_coords, non_outer_wall_coords, walls_grid, None, allLegalSuccessorAxioms))
     KB.append(pacphysicsAxioms(1, all_coords, non_outer_wall_coords, walls_grid, None, allLegalSuccessorAxioms))
     KB.append(PropSymbolExpr(pacman_str, x0, y0, time=0))
